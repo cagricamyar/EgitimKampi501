fc539165126d0dd25a29c5667948b5adbe05a144
çağrı çamyar <cagri-95@hotmail.com>
2024-12-24 23:33:57 +0300

    Ders22 sonu

--- EgitimKampi501/Form1.Designer.cs
+++ EgitimKampi501/Form1.Designer.cs
@@ -31,18 +31,24 @@
 			this.label1 = new System.Windows.Forms.Label();
 			this.dataGridView1 = new System.Windows.Forms.DataGridView();
 			this.btnList = new System.Windows.Forms.Button();
-			this.textBox1 = new System.Windows.Forms.TextBox();
-			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.txtProductId = new System.Windows.Forms.TextBox();
+			this.txtProductName = new System.Windows.Forms.TextBox();
 			this.label2 = new System.Windows.Forms.Label();
-			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.txtProductStock = new System.Windows.Forms.TextBox();
 			this.label3 = new System.Windows.Forms.Label();
-			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.txtProductPrice = new System.Windows.Forms.TextBox();
 			this.label4 = new System.Windows.Forms.Label();
-			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.txtProductCategory = new System.Windows.Forms.TextBox();
 			this.label5 = new System.Windows.Forms.Label();
 			this.btnAdd = new System.Windows.Forms.Button();
 			this.btnUpdate = new System.Windows.Forms.Button();
 			this.btnDelete = new System.Windows.Forms.Button();
+			this.lblTotal = new System.Windows.Forms.Label();
+			this.lblTotalProductCount = new System.Windows.Forms.Label();
+			this.lblMaxPriceProductName = new System.Windows.Forms.Label();
+			this.lblMaxPriceProduct = new System.Windows.Forms.Label();
+			this.lblDistinctCategoryCount = new System.Windows.Forms.Label();
+			this.lblDistinctCategory = new System.Windows.Forms.Label();
 			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
 			this.SuspendLayout();
 			// 
@@ -74,19 +80,19 @@
 			this.btnList.UseVisualStyleBackColor = true;
 			this.btnList.Click += new System.EventHandler(this.btnList_Click);
 			// 
-			// textBox1
+			// txtProductId
 			// 
-			this.textBox1.Location = new System.Drawing.Point(109, 60);
-			this.textBox1.Name = "textBox1";
-			this.textBox1.Size = new System.Drawing.Size(158, 20);
-			this.textBox1.TabIndex = 3;
+			this.txtProductId.Location = new System.Drawing.Point(109, 60);
+			this.txtProductId.Name = "txtProductId";
+			this.txtProductId.Size = new System.Drawing.Size(158, 20);
+			this.txtProductId.TabIndex = 3;
 			// 
-			// textBox2
+			// txtProductName
 			// 
-			this.textBox2.Location = new System.Drawing.Point(109, 86);
-			this.textBox2.Name = "textBox2";
-			this.textBox2.Size = new System.Drawing.Size(158, 20);
-			this.textBox2.TabIndex = 5;
+			this.txtProductName.Location = new System.Drawing.Point(109, 86);
+			this.txtProductName.Name = "txtProductName";
+			this.txtProductName.Size = new System.Drawing.Size(158, 20);
+			this.txtProductName.TabIndex = 5;
 			// 
 			// label2
 			// 
@@ -97,12 +103,12 @@
 			this.label2.TabIndex = 4;
 			this.label2.Text = "Urun Adi:";
 			// 
-			// textBox3
+			// txtProductStock
 			// 
-			this.textBox3.Location = new System.Drawing.Point(109, 138);
-			this.textBox3.Name = "textBox3";
-			this.textBox3.Size = new System.Drawing.Size(158, 20);
-			this.textBox3.TabIndex = 9;
+			this.txtProductStock.Location = new System.Drawing.Point(109, 138);
+			this.txtProductStock.Name = "txtProductStock";
+			this.txtProductStock.Size = new System.Drawing.Size(158, 20);
+			this.txtProductStock.TabIndex = 9;
 			// 
 			// label3
 			// 
@@ -113,12 +119,12 @@
 			this.label3.TabIndex = 8;
 			this.label3.Text = "Stok:";
 			// 
-			// textBox4
+			// txtProductPrice
 			// 
-			this.textBox4.Location = new System.Drawing.Point(109, 112);
-			this.textBox4.Name = "textBox4";
-			this.textBox4.Size = new System.Drawing.Size(158, 20);
-			this.textBox4.TabIndex = 7;
+			this.txtProductPrice.Location = new System.Drawing.Point(109, 112);
+			this.txtProductPrice.Name = "txtProductPrice";
+			this.txtProductPrice.Size = new System.Drawing.Size(158, 20);
+			this.txtProductPrice.TabIndex = 7;
 			// 
 			// label4
 			// 
@@ -129,12 +135,12 @@
 			this.label4.TabIndex = 6;
 			this.label4.Text = "Fiyat:";
 			// 
-			// textBox5
+			// txtProductCategory
 			// 
-			this.textBox5.Location = new System.Drawing.Point(109, 164);
-			this.textBox5.Name = "textBox5";
-			this.textBox5.Size = new System.Drawing.Size(158, 20);
-			this.textBox5.TabIndex = 11;
+			this.txtProductCategory.Location = new System.Drawing.Point(109, 164);
+			this.txtProductCategory.Name = "txtProductCategory";
+			this.txtProductCategory.Size = new System.Drawing.Size(158, 20);
+			this.txtProductCategory.TabIndex = 11;
 			// 
 			// label5
 			// 
@@ -153,6 +159,7 @@
 			this.btnAdd.TabIndex = 12;
 			this.btnAdd.Text = "Ekle";
 			this.btnAdd.UseVisualStyleBackColor = true;
+			this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
 			// 
 			// btnUpdate
 			// 
@@ -162,6 +169,7 @@
 			this.btnUpdate.TabIndex = 14;
 			this.btnUpdate.Text = "Guncelle";
 			this.btnUpdate.UseVisualStyleBackColor = true;
+			this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
 			// 
 			// btnDelete
 			// 
@@ -171,30 +179,101 @@
 			this.btnDelete.TabIndex = 13;
 			this.btnDelete.Text = "Sil";
 			this.btnDelete.UseVisualStyleBackColor = true;
+			this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+			// 
+			// lblTotal
+			// 
+			this.lblTotal.AutoSize = true;
+			this.lblTotal.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.lblTotal.Location = new System.Drawing.Point(75, 502);
+			this.lblTotal.Name = "lblTotal";
+			this.lblTotal.Size = new System.Drawing.Size(192, 26);
+			this.lblTotal.TabIndex = 15;
+			this.lblTotal.Text = "Toplam Kitap Sayısı : ";
+			// 
+			// lblTotalProductCount
+			// 
+			this.lblTotalProductCount.AutoSize = true;
+			this.lblTotalProductCount.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.lblTotalProductCount.ForeColor = System.Drawing.Color.Gold;
+			this.lblTotalProductCount.Location = new System.Drawing.Point(268, 502);
+			this.lblTotalProductCount.Name = "lblTotalProductCount";
+			this.lblTotalProductCount.Size = new System.Drawing.Size(23, 26);
+			this.lblTotalProductCount.TabIndex = 16;
+			this.lblTotalProductCount.Text = "0";
+			// 
+			// lblMaxPriceProductName
+			// 
+			this.lblMaxPriceProductName.AutoSize = true;
+			this.lblMaxPriceProductName.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.lblMaxPriceProductName.Location = new System.Drawing.Point(75, 528);
+			this.lblMaxPriceProductName.Name = "lblMaxPriceProductName";
+			this.lblMaxPriceProductName.Size = new System.Drawing.Size(155, 26);
+			this.lblMaxPriceProductName.TabIndex = 17;
+			this.lblMaxPriceProductName.Text = "En Pahali Kitap : ";
+			// 
+			// lblMaxPriceProduct
+			// 
+			this.lblMaxPriceProduct.AutoSize = true;
+			this.lblMaxPriceProduct.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.lblMaxPriceProduct.ForeColor = System.Drawing.Color.Gold;
+			this.lblMaxPriceProduct.Location = new System.Drawing.Point(268, 528);
+			this.lblMaxPriceProduct.Name = "lblMaxPriceProduct";
+			this.lblMaxPriceProduct.Size = new System.Drawing.Size(23, 26);
+			this.lblMaxPriceProduct.TabIndex = 18;
+			this.lblMaxPriceProduct.Text = "0";
+			// 
+			// lblDistinctCategoryCount
+			// 
+			this.lblDistinctCategoryCount.AutoSize = true;
+			this.lblDistinctCategoryCount.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.lblDistinctCategoryCount.ForeColor = System.Drawing.Color.Gold;
+			this.lblDistinctCategoryCount.Location = new System.Drawing.Point(268, 554);
+			this.lblDistinctCategoryCount.Name = "lblDistinctCategoryCount";
+			this.lblDistinctCategoryCount.Size = new System.Drawing.Size(23, 26);
+			this.lblDistinctCategoryCount.TabIndex = 20;
+			this.lblDistinctCategoryCount.Text = "0";
+			// 
+			// lblDistinctCategory
+			// 
+			this.lblDistinctCategory.AutoSize = true;
+			this.lblDistinctCategory.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.lblDistinctCategory.Location = new System.Drawing.Point(75, 554);
+			this.lblDistinctCategory.Name = "lblDistinctCategory";
+			this.lblDistinctCategory.Size = new System.Drawing.Size(148, 26);
+			this.lblDistinctCategory.TabIndex = 19;
+			this.lblDistinctCategory.Text = "Kategori Sayisi :";
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.BackColor = System.Drawing.Color.MediumSeaGreen;
-			this.ClientSize = new System.Drawing.Size(1150, 428);
+			this.ClientSize = new System.Drawing.Size(1150, 770);
+			this.Controls.Add(this.lblDistinctCategoryCount);
+			this.Controls.Add(this.lblDistinctCategory);
+			this.Controls.Add(this.lblMaxPriceProduct);
+			this.Controls.Add(this.lblMaxPriceProductName);
+			this.Controls.Add(this.lblTotalProductCount);
+			this.Controls.Add(this.lblTotal);
 			this.Controls.Add(this.btnUpdate);
 			this.Controls.Add(this.btnDelete);
 			this.Controls.Add(this.btnAdd);
-			this.Controls.Add(this.textBox5);
+			this.Controls.Add(this.txtProductCategory);
 			this.Controls.Add(this.label5);
-			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.txtProductStock);
 			this.Controls.Add(this.label3);
-			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.txtProductPrice);
 			this.Controls.Add(this.label4);
-			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.txtProductName);
 			this.Controls.Add(this.label2);
-			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.txtProductId);
 			this.Controls.Add(this.btnList);
 			this.Controls.Add(this.dataGridView1);
 			this.Controls.Add(this.label1);
 			this.Name = "Form1";
 			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
 			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
 			this.ResumeLayout(false);
 			this.PerformLayout();
@@ -206,18 +285,24 @@
 		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.DataGridView dataGridView1;
 		private System.Windows.Forms.Button btnList;
-		private System.Windows.Forms.TextBox textBox1;
-		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox txtProductId;
+		private System.Windows.Forms.TextBox txtProductName;
 		private System.Windows.Forms.Label label2;
-		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox txtProductStock;
 		private System.Windows.Forms.Label label3;
-		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.TextBox txtProductPrice;
 		private System.Windows.Forms.Label label4;
-		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.TextBox txtProductCategory;
 		private System.Windows.Forms.Label label5;
 		private System.Windows.Forms.Button btnAdd;
 		private System.Windows.Forms.Button btnUpdate;
 		private System.Windows.Forms.Button btnDelete;
+		private System.Windows.Forms.Label lblTotal;
+		private System.Windows.Forms.Label lblTotalProductCount;
+		private System.Windows.Forms.Label lblMaxPriceProductName;
+		private System.Windows.Forms.Label lblMaxPriceProduct;
+		private System.Windows.Forms.Label lblDistinctCategoryCount;
+		private System.Windows.Forms.Label lblDistinctCategory;
 	}
 }
 
--- EgitimKampi501/Form1.cs
+++ EgitimKampi501/Form1.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Dapper;
+using EgitimKampi501.Dtos;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -16,10 +19,64 @@ namespace EgitimKampi501
 		{
 			InitializeComponent();
 		}
-
-		private void btnList_Click(object sender, EventArgs e)
+		
+		SqlConnection connection = new SqlConnection("Server=(localdb)\\mssqllocaldb; initial Catalog = EgitimKampi501Db; integrated security = true");
+		private async void btnList_Click(object sender, EventArgs e)
 		{
+			string query = "Select * from TblProduct";
+			var values = await connection.QueryAsync<ResultProductDto>(query);
+			dataGridView1.DataSource = values;
+		}
 
+		private async void btnAdd_Click(object sender, EventArgs e)
+		{
+			string query = "insert into TblProduct (ProductName, ProductStock, ProductPrice,ProductCategory)" +
+				"values (@productName, @productStock, @productPrice, @productCategory)";
+			var parameters = new DynamicParameters();
+			parameters.Add("@productName", txtProductName.Text);
+			parameters.Add("@productStock", txtProductStock.Text);
+			parameters.Add("@productPrice", txtProductPrice.Text);
+			parameters.Add("@productCategory", txtProductCategory.Text);
+			await connection.ExecuteAsync(query, parameters);
+			MessageBox.Show("Yeni Kitap Ekleme Basarili");
+		}
+
+		private async void btnDelete_Click(object sender, EventArgs e)
+		{
+			string query = "delete from TblProduct where ProductId = @productId";
+			var parameters = new DynamicParameters();
+			parameters.Add("@productId", txtProductId.Text);
+			await connection.ExecuteAsync(query, parameters);
+			MessageBox.Show("Kitap Silme Basarili");
+		}
+
+		private async void btnUpdate_Click(object sender, EventArgs e)
+		{
+			string query = "update TblProduct set ProductName = @productName, ProductPrice = @productPrice, " +
+				"ProductStock = @productStock, ProductCategory = @productCategory where ProductId = @productId";
+			var parameters = new DynamicParameters();
+			parameters.Add("@productId", txtProductId.Text);
+			parameters.Add("@productName", txtProductName.Text);
+			parameters.Add("@productStock", txtProductStock.Text);
+			parameters.Add("@productPrice", txtProductPrice.Text);
+			parameters.Add("@productCategory", txtProductCategory.Text);
+			await connection.ExecuteAsync(query, parameters);
+			MessageBox.Show("Kitap guncelleme islemi basarili",  "Guncelleme", MessageBoxButtons.OK, MessageBoxIcon.Information);
+		}
+
+		private async void Form1_Load(object sender, EventArgs e)
+		{
+			string query1 = "Select count(*) from TblProduct";
+			var productTotalCount = await connection.QueryFirstOrDefaultAsync<int>(query1);
+			lblTotalProductCount.Text = productTotalCount.ToString();
+
+			string query2 = "Select productName from TblProduct where productPrice = (Select max(productPrice) from tblproduct)";
+			var maxPriceProductName = await connection.QueryFirstOrDefaultAsync<string>(query2);
+			lblMaxPriceProduct.Text = maxPriceProductName.ToString();
+
+			string query3 = "Select count(distinct(productCategory)) from tblProduct";
+			var distinctProductCount = await connection.QueryFirstOrDefaultAsync<int>(query3);
+			lblDistinctCategoryCount.Text = distinctProductCount.ToString();
 		}
 	}
 }
